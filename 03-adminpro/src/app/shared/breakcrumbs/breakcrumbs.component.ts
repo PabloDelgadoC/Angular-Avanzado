5d088a45469dd077fbbf85cc0c78a882c3b7a037
Pablo Delgado <andresdelgadocampoverde@gmail.com>
2021-07-11 20:34:29 -0500

    Creacion de componentes y rutas raiz e hijas en el proyecto AdminPro

---
+++ 03-adminpro/src/app/shared/breakcrumbs/breakcrumbs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breakcrumbs',
+  templateUrl: './breakcrumbs.component.html',
+  styles: [
+  ]
+})
+export class BreakcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
